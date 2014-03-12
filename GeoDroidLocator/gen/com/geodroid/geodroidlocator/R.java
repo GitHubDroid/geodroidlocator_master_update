b8db54abcab6611ef8a4eb6a3cedb7a0d77d6368
GitHubDroid <promptnet@promptnetworks.com>
2014-03-11 20:22:46 -0400

    Activities Setup Work

--- GeoDroidLocator/gen/com/geodroid/geodroidlocator/R.java
+++ GeoDroidLocator/gen/com/geodroid/geodroidlocator/R.java
@@ -18,11 +18,11 @@ public final class R {
     }
     public static final class id {
         public static final int goButton=0x7f070000;
-        public static final int huisNummerTextView=0x7f070005;
+        public static final int houseNumberTextView=0x7f070005;
         public static final int menuAbout=0x7f070006;
         public static final int numberSpinner=0x7f070003;
-        public static final int straatnaamTextView=0x7f070004;
         public static final int streetNameTextField=0x7f070002;
+        public static final int streetnameTextView=0x7f070004;
         public static final int textView2=0x7f070001;
     }
     public static final class layout {
@@ -32,8 +32,7 @@ public final class R {
         public static final int menu=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
--- GeoDroidLocator/src/com/geodroid/geodroidlocator/SimpleLocationOverlay.java
+++
@@ -1,5 +0,0 @@
-package com.geodroid.geodroidlocator;
-
-public class SimpleLocationOverlay {
-
-}
